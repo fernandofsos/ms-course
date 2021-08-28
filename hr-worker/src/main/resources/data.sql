b341373811f23444df5943235f53781e05b36ff6
Sergio Oliveira <fernandofsos@gmail.comm>
2021-08-27 21:27:41 -0300

    atualização do ms-workers

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into TB_WORKER (name,daily_Icome) values ('Bob',200.0);
+insert into TB_WORKER (name,daily_Icome) values ('Maria',300.0);
+insert into TB_WORKER (name,daily_Icome) values ('Alex',250.0);
