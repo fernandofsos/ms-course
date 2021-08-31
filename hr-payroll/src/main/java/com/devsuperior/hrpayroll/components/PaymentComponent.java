bbc464de3848805a8a465fa31ae5472b8e007cb1
Sergio Oliveira <fernandofsos@gmail.comm>
2021-08-31 12:27:35 -0300

    feign client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/components/PaymentComponent.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/components/PaymentComponent.java
@@ -1,37 +1,23 @@
 package com.devsuperior.hrpayroll.components;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 @Component
 public class PaymentComponent {
-	
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
-	
-	@Autowired
-	RestTemplate restTemplate;
 
-	public Payment payment(long workerId, int days ) {
-		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id",  Long.toString(workerId) );
-		
-		Worker worker = restTemplate.getForObject(workerHost +"/workers/{id}", Worker.class, uriVariables);
-		
-		return new Payment(worker.getName(), worker.getDailyIcome(),days);
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
+	public Payment payment(long workerId, int days) {
+
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+
+		return new Payment(worker.getName(), worker.getDailyIcome(), days);
 	}
 
-	
-	
 }
